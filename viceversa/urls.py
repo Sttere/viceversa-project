777a8a9fa38917eab12afd645666f7213b34a323
Stas <sttere@mail.ru>
2021-04-06 16:09:11 +0500

    Add reverse feature

--- viceversa/urls.py
+++ viceversa/urls.py
@@ -18,4 +18,5 @@ from .import view
 
 urlpatterns = [
     path('', view.home),
+    path('reversed/', view.reverse, name='reverse'),
 ]
--- viceversa/view.py
+++ viceversa/view.py
@@ -2,3 +2,9 @@ from django.shortcuts import render
 
 def home(request):
 	return render(request, 'home.html')
+
+def reverse(request):
+	user_text = request.GET['usertext']
+	reversed_text = user_text[::-1]
+	return render(request, 'reverse.html', {'usertext': user_text, 'reversedtext':reversed_text})
+
